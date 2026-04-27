ad62a2d0f547fde9ac166feba2f9288e61a3eb5f
Zshumake <shumaker.zach865@gmail.com>
2026-04-27 16:43:13 -0400

    Deploy: legend fix + GH anterior position photo crop

---
+++ three_viewer/serve.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import http.server, os, sys
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+port = int(sys.argv[1]) if len(sys.argv) > 1 else 8090
+http.server.HTTPServer(('', port), http.server.SimpleHTTPRequestHandler).serve_forever()
